4d8fdba71e163dab53bfe76f6ce12d1da2a34a22
gasc <mayeul.gasc@orange.fr>
2019-09-26 19:13:36 +0200

    Ajout d'un fonction test des fonctions de tri

--- Test_Tri/test_tri.adb
+++ Test_Tri/test_tri.adb
@@ -7,10 +7,10 @@ procedure Test_Tri is
    
    -- Type tableau 
    type nbr is array (Integer range <>) of Integer;
-   type Pf is access function-- cd openc
+   type P_procedure is access procedure (Tab : in out Nbr);
    
    -- Procédure de tri d'un tableau
-   procedure tri (Tab : in out Nbr) is
+   procedure Tri_Bulle (Tab : in out Nbr) is
       K : Integer := 0; -- buffer
    begin
       for J in Tab'Range loop
@@ -22,15 +22,15 @@ procedure Test_Tri is
 	    end if;
 	 end loop;
       end loop;
-   end tri;
+   end Tri_Bulle;
    
    -- Test de la procédure tri
-   procedure Test_Tri  is
+   procedure Test_Tri (F : P_Procedure) is
       test_tri1 : nbr(1..10) := (5,4,8,3,9,1,6,7,2,0);
       test_tri2 : nbr(1..10) := (5,4,8,6,9,1,6,7,7,0);
    begin
-      tri(test_tri2);
-      tri(test_tri1);
+      F(test_tri2);
+      F(test_tri1);
 
       for i in test_tri1'First..test_tri1'Last loop
          put(Integer'Image(test_tri1(i))& " ");
@@ -40,7 +40,7 @@ procedure Test_Tri is
          put(Integer'Image(test_tri2(i))& " ");
       end loop;
       Put_Line("");
-   end test_tri;
+   end Test_tri;
    
    
    function Factorielle(N : Integer) return Integer is
@@ -54,11 +54,11 @@ procedure Test_Tri is
       return Resultat;
    end Factorielle;
    
-   -- Main
-   N : Integer;
+   F : P_Procedure;
 begin
-   -- test_Tri;
+   F := Tri_Bulle'Access;
+   Test_Tri(F);
    
-   Ada.Integer_Text_IO.Get(N);
-   Put_Line(Integer'Image(Factorielle(N)));
+   --Ada.Integer_Text_IO.Get(N);
+   --Put_Line(Integer'Image(Factorielle(N)));
 end Test_Tri;
