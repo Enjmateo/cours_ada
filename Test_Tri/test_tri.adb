322f500f6e94111a6bd750bb114bee4da14deb9a
gasc <mayeul.gasc@orange.fr>
2019-10-04 17:46:03 +0200

    Ajout d'un affichage graphique pour test_tri

--- Test_Tri/test_tri.adb
+++ Test_Tri/test_tri.adb
@@ -1,4 +1,5 @@
 with ada.Text_IO, Ada.Integer_Text_IO;
+with Ada.Numerics.Discrete_Random;
 with ada.Calendar;
 
 use ada.Text_IO;
@@ -7,7 +8,48 @@ procedure Test_Tri is
    
    -- Type tableau 
    type nbr is array (Integer range <>) of Integer;
+   type Grille is array (Integer range <>, Integer range <>) of character;
+   subtype Double is Integer range 0..65535;
    type P_procedure is access procedure (Tab : in out Nbr);
+   
+   -- Package pour l'aléatoire
+   package Aleatoire is new Ada.Numerics.Discrete_Random(Double);
+   use Aleatoire;
+   Hasard : Generator;
+   
+   -- Procédure d'affichage d'un tableau de manière graphique
+   procedure Aff (Tab : in  Nbr) is
+      Hauteur : constant Integer := Tab'Last;
+      Value : Integer := 0;
+      G : Grille(Tab'Range, 1..Hauteur) := (others => (others => ' '));
+      Max : Integer := 0;
+   begin
+      -- Recherche du max
+      for K in Tab'Range loop
+	 if Value < Tab(K) then
+	    Max := K;
+	    Value := Tab(K);
+	 end if;
+      end loop;
+      
+      Value := 0;
+      
+      -- Préparation affichage
+      for I in Tab'Range loop
+	 Value := ((Tab(I)*Tab'Last)/(Tab(Max)+1));
+	 for J in G'First(2)..Value loop
+	    G(J,I) := '#';
+	 end loop;
+      end loop;
+      
+      --Affichage
+      for H in G'Range(1) loop
+	 for V in G'Range(2) loop
+	    Put(G(G'Last-H+1,V) & " ");
+	 end loop;
+	 New_Line;
+      end loop;
+   end Aff;
    
    -- Procédure de tri d'un tableau (Tri bulle)
    procedure Tri_Bulle (Tab : in out Nbr) is
@@ -45,25 +87,27 @@ procedure Test_Tri is
    
    -- Test de la procédure tri
    procedure Test_Tri (F : P_Procedure) is
-      test_tri1 : nbr(1..10) := (5,4,8,3,9,1,6,7,2,0);
-      test_tri2 : nbr(1..10) := (5,4,8,6,9,1,6,7,7,0);
+      test_tri1 : nbr(1..40);
    begin
-      F(test_tri2);
+      Put_Line("------------------");
+      Put_Line("TEST TRI");
+      for J in Test_Tri1'Range loop
+	 Test_Tri1(J) := Random(Hasard);
+      end loop;
+      
+      Aff(Test_Tri1);
       F(test_tri1);
-      for i in test_tri1'First..test_tri1'Last loop
-         put(Integer'Image(test_tri1(i))& " ");
-      end loop;
-      Put_Line("");
-      for i in test_tri2'First..test_tri2'Last loop
-         put(Integer'Image(test_tri2(i))& " ");
-      end loop;
-      Put_Line("");
+      Aff(Test_Tri1);
    end Test_tri;
    
    
    F1 : P_Procedure;
    F2 : P_Procedure;
 begin
+   --Renitialisation du générateur
+   Reset(Hasard);
+   
+   --Test des fonctions
    F1 := Tri_Bulle'Access;
    F2 := Tri_Bulle'Access;
    Test_Tri(F1); Test_Tri(F2);
