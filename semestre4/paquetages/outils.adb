cb62b8b72107ebdc64c9cb3e21155c7375b631bc
gasc <mayeul.gasc@orange.fr>
2020-02-07 13:39:14 +0100

    Ajout séance 3

---
+++ semestre4/paquetages/outils.adb
@@ -0,0 +1,20 @@
+package body Outils is
+   function Pgcd(A, B : in Positive) return Positive is 
+      P : Integer := 1;
+   begin
+      for I in 1..A loop
+	 if A mod I = 0 and B mod I = 0 then
+	    P := I;
+	 end if;
+      end loop;
+      return P;
+   end Pgcd;
+   
+   function Ppcm(A, B : in Positive) return Positive is 
+   begin
+      return 1;
+   end Ppcm;
+   
+begin
+   null;
+end Outils;
--- semestre4/paquetages/rationnels.adb
+++ semestre4/paquetages/rationnels.adb
@@ -1,3 +1,60 @@
 with Ada.Text_Io; use Ada.Text_Io;
+with Outils; use Outils;
 
-package body Rationnel
+package body Rationnels is
+   
+   function Simplify(A : Rationnel) return Rationnel is
+      D : Integer := Pgcd(A.Num, A.Den);
+   begin
+      return (A.Num / D, A.Den / D);
+   end Simplify;
+   
+   function Constructeur(A, B : in Integer) return Rationnel is
+      C : Rationnel := (A,B);
+   begin
+      return Simplify(C);
+   end Constructeur;
+   
+   function Numerateur(A : in Rationnel) return Integer is
+   begin
+      return A.Num;
+   end Numerateur;
+   
+   function Denominateur(A : in Rationnel) return Integer is
+   begin
+      return A.Den;
+   end Denominateur;
+   
+   --Opérations arithmétiques
+   function "="(A, B : in Rationnel) return Boolean is
+   begin
+      return A.Num = B.Num and A.Den = B.Den;
+   end "=";
+   
+   function "+"(A, B : in Rationnel) return Rationnel is
+   begin
+      return Simplify((A.Num * B.Den + A.Den * B.Num, A.Den * B.Den));
+   end "+";
+     
+   function "-"(A, B : in Rationnel) return Rationnel is
+   begin
+      return A + ((-1)*B.Num, B.Den);
+   end "-";
+   
+   function "*"(A, B : in Rationnel) return Rationnel is
+   begin
+      return (A.Num * B.Num, A.Den * B.Den);
+   end "*";
+   
+   function "/"(A, B : in Rationnel) return Rationnel is
+   begin
+      return (A.Num * B.Den, A.Den * B.Num);
+   end "/";
+   function To_String(A : in Rationnel) return String is
+   begin
+      return (" " & Integer'Image(A.Num) & " / " & Integer'Image(A.Den) & " ");
+   end To_String;
+   
+begin
+   null;
+end Rationnels;
