7bdf38f9e82a0503f3ad840c954b1334e342c787
gasc <mayeul.gasc@orange.fr>
2020-02-11 08:30:46 +0100

    Travaux sur TD3

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 Partage de mes essais en Ada dans le cadre des cours de 2ème année MIC à l'INSA Toulouse et du [cours d'openclassroom](https://openclassrooms.com/fr/courses/900279-apprenez-a-programmer-avec-ada).
 Le code est sous licence GNU GPL 3, les cours de l'INSA et d'openclassroom appartiennent à leurs propriétaires respectifs.
 
-### Cours INSA du semestre 4
+### Index - Cours INSA du semestre 4
 * TD1 : Pointeurs et listes :
 	* semestre4/listes
 	* semestre4/listes_doulement_chaine
--- semestre4/listes_ordonnees/listes.adb
+++ semestre4/listes_ordonnees/listes.adb
@@ -21,6 +21,7 @@ package body Listes is
    end Init;
    
    function Appartient(E : in Integer; L : in Liste) return Boolean;
+   
    procedure Inserer(E : Integer; L : in out Liste) is
       P : Liste;
    begin
@@ -34,11 +35,29 @@ package body Listes is
 	 end if;
       else
 	 P := L;
-	 while P.all.Suiv.all.Info < E and 
+	 while P.all.Suiv /= null and then P.all.Suiv.all.Info < E loop
+	    P := P.all.Suiv;
+	 end loop;
+	 if P.all.Suiv = null then
+	    P.all.Suiv := new Cellule'(E, null);
+	 else
+	    P.all.Suiv := new Cellule'(E, P.all.Suiv.all.Suiv);
+	 end if;
       end if;
    end Inserer;
    
-   procedure Supprimer(E : Integer; L : in out Liste);
+   procedure Supprimer(E : Integer; L : in out Liste) is
+   begin
+      if Appartient(E, L) then
+	 if L.all.Info = E then
+	    L := L.all.Suiv;
+	 else
+	    if L.all.Suiv.all.Info = E then
+	       L.all.Suiv := L.all.Suiv.all.Suiv;
+	    else
+	       Supprimer(E, L.all.Suiv);
+	    end if;
+	 end if;
    procedure Afficher(L : in Liste);
    
 begin
